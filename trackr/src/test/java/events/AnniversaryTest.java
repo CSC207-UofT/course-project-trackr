6b11bf7f394c31d3e4ea4f17abfd8f2620eb2ef9
Roman Zupancic <roman.k.zupancic@gmail.com>
2021-10-09 18:23:37 -0400

    Implemented Tests for Events, tests not yet tested

--- trackr/src/test/java/events/AnniversaryTest.java
+++ trackr/src/test/java/events/AnniversaryTest.java
@@ -1,7 +1,43 @@
 package events;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AnniversaryTest {
 
+    @Test
+    void testReminderDatesTrue() {
+        Anniversary anniversary = new Anniversary(
+                "Wedding",
+                new Date(2021, 11, 10),
+                new Date(2021, 11, 5),
+                new Person());
+
+        assertTrue(anniversary.isReminderDeadline(new Date(2021, 11, 5)));
+    }
+
+    @Test
+    void testReminderDatesFalse() {
+        Anniversary anniversary = new Anniversary(
+                "Wedding",
+                new Date(2021, 11, 10),
+                new Date(2021, 11, 5),
+                new Person());
+
+        assertTrue(anniversary.isReminderDeadline(new Date(2021, 11, 10)));
+    }
+
+    @Test
+    void testReminderDatesNull() {
+        Anniversary anniversary = new Anniversary(
+                "Wedding",
+                new Date(2021, 11, 10),
+                new Person());
+
+        assertTrue(anniversary.isReminderDeadline(new Date(2021, 11, 10)));
+    }
 }
--- trackr/src/test/java/events/BirthdayTest.java
+++ trackr/src/test/java/events/BirthdayTest.java
@@ -1,7 +1,42 @@
 package events;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BirthdayTest {
 
+    @Test
+    void testReminderDatesTrue() {
+        Birthday birthday = new Birthday(
+                "Wedding",
+                new Date(2021, 11, 10),
+                new Date(2021, 11, 5),
+                new Person());
+
+        assertTrue(birthday.isReminderDeadline(new Date(2021, 11, 5)));
+    }
+
+    @Test
+    void testReminderDatesFalse() {
+        Birthday birthday = new Birthday(
+                "Wedding",
+                new Date(2021, 11, 10),
+                new Date(2021, 11, 5),
+                new Person());
+
+        assertTrue(birthday.isReminderDeadline(new Date(2021, 11, 10)));
+    }
+
+    @Test
+    void testReminderDatesNull() {
+        Birthday birthday = new Birthday(
+                "Wedding",
+                new Date(2021, 11, 10),
+                new Person());
+
+        assertTrue(birthday.isReminderDeadline(new Date(2021, 11, 10)));
+    }
 }
