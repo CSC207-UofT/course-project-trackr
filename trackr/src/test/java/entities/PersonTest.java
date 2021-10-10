b391453a87e25f6157a87bc2804d991b1be0114a
Menghao Yu <menghaoyu2002@gmail.com>
2021-10-10 00:35:32 -0400

    Added tests for Person class

---
+++ trackr/src/test/java/entities/PersonTest.java
@@ -0,0 +1,37 @@
+package entities;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+    private Person person;
+
+    @BeforeEach
+    void setUp() {
+        this.person = new Person("David");
+    }
+
+    @Test
+    public void testAddTagSuccess() {
+        assertTrue(this.person.addTag("Cool"));
+    }
+
+    @Test
+    public void testAddExitingTag() {
+        this.person.addTag("Cool");
+        assertFalse(this.person.addTag("Cool"));
+    }
+
+    @Test
+    public void testRemoveExistingTag() {
+        this.person.addTag("Cool");
+        assertTrue(this.person.removeTag("Cool"));
+    }
+
+    @Test
+    public void testRemoveNonexistentTag() {
+        assertFalse(this.person.removeTag("Cool"));
+    }
+}
