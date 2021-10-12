6bf6b1903778a4d4b7a4d1031d0b4d8d995d2a5a
Jonathan <jonathanginevro@outlook.com>
2021-10-11 23:12:34 -0400

    Implemented PersonManager class

---
+++ trackr/src/main/java/UseCases/PersonManager.java
@@ -0,0 +1,39 @@
+package UseCases;
+import entities.Person;
+
+import java.util.HashMap;
+
+public class PersonManager {
+    private HashMap<String, Object> eventMap;
+
+    public PersonManager() {
+        this.eventMap = new HashMap<>();
+    }
+
+    public boolean addPerson(String name, Person value){
+        if (this.eventMap.containsKey(name)){
+            return false;
+        } else {
+            this.eventMap.put(name, value);
+            return true;
+        }
+    }
+
+    public boolean removePerson(String name){
+        if (!this.eventMap.containsKey(name)){
+            return false;
+        } else {
+            this.eventMap.remove(name);
+            return true;
+        }
+    }
+
+    public boolean editPerson(String name, Person newValue){
+        if (!this.eventMap.containsKey(name)){
+            return false;
+        } else {
+            this.eventMap.put(name, newValue);
+            return true;
+        }
+    }
+}
