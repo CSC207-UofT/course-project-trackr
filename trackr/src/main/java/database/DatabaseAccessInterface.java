275c5b21d2944402aa7c1d1ba8088105a44f7006
Menghao Yu <menghaoyu2002@gmail.com>
2021-10-15 15:44:05 -0400

    Removed Database operations regarding the PersonData.

--- trackr/src/main/java/database/DatabaseAccessInterface.java
+++ trackr/src/main/java/database/DatabaseAccessInterface.java
@@ -12,30 +12,10 @@ import java.util.List;
  */
 public interface DatabaseAccessInterface {
     /**
-     * Return a List of Person objects representing all the Person objects in the Database
-     * @return a List of all Person objects in the Database
-     */
-    List<Person> getPersonData();
-
-    /**
      * Return a List of all Events in the Database
      * @return a list of all Events in the Database
      */
     List<Event> getEventData();
-
-    /**
-     * Add a new Person to the Database
-     * @param person the Person to add to the Database
-     */
-    void addPerson(Person person);
-
-    /**
-     * Remove an existing Person from the database. Return a boolean representing if the
-     * Person was successfully removed or not.
-     * @param person the person to remove from the database
-     * @return return a boolean representing if the person was successfully removed or not
-     */
-    boolean removePerson(Person person);
 
     /**
      * Add a new Event to the Database
@@ -50,15 +30,6 @@ public interface DatabaseAccessInterface {
      * @return the boolean indicating if the Event was removed or not.
      */
     boolean removeEvent(Event event);
-
-    /**
-     * Return a List of all Person objects in the database that have
-     * the specified first and last names
-     * @param firstName the first name of the person to search for
-     * @param lastName the last name of the person to search for
-     * @return A List of all Person objects
-     */
-    List<Person> findPerson(String firstName, String lastName);
 
     /**
      * Return a List of Events in the Database which have a Person with the
