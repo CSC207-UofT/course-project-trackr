14bd455a4b9a10b8fdf645c8eb32df84ada21e73
Menghao Yu <menghaoyu2002@gmail.com>
2021-10-15 00:58:57 -0400

    Added documentation for the Person class

--- trackr/src/test/java/entities/PersonTest.java
+++ trackr/src/test/java/entities/PersonTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * A class representing the target Person for an Event.
+ */
 class PersonTest {
     private Person person;
 
