c4836a021e551d4455bd851e41abf84310df994e
danielhocevar <danielhocevar1@gmail.com>
2021-10-15 01:07:59 -0400

    Added EventManager class and addEvent and deleteEvent methods.

--- trackr/src/main/java/usecases/EventManager.java
+++ trackr/src/main/java/usecases/EventManager.java
@@ -1,46 +1,59 @@
 package usecases;
 
-import java.util.HashMap;
+import database.DatabaseAccessInterface;
+import entities.Anniversary;
+import entities.Birthday;
+import entities.Event;
+import entities.Person;
+
+import java.time.LocalDate;
+import java.util.List;
 
 /**
  * EventManager is a use case class that manages the creation and deletion of events
  */
 public class EventManager {
-    //Define a hashmap to store the events
-    private HashMap<String, Object> eventMap;
 
-    public EventManager() {
-        this.eventMap = new HashMap<>();
+    private DatabaseAccessInterface dataAccessor;
+
+    public EventManager(DatabaseAccessInterface dataAccessorObj) {
+        this.dataAccessor = dataAccessorObj;
     }
 
-    public HashMap<String, Object> getAllEvents() {
-        return this.eventMap;
+    public List<Event> getAllEvents() {
+        return dataAccessor.getEventData();
     }
 
     /**
-     * Add an event to the event hash map
-     * @param keyName - the key we are adding to the hashmap
-     * @param val - the value corresponding to the key
+     * Add an event to the database
+     * @param person - the key we are adding to the hashmap
+     * @param eventTyoe - a string representing the type of event (either Birthday or Anniversary)
+     * @param date - the date of the event
+     * @param reminderDeadline - the date to innitiate a reminder
      * @return true if the key is not already in the hashmap and false otherwise
      */
-    public boolean addEvent(String keyName, Object val) {
-        if (eventMap.containsKey(keyName)) {
-            return false;
+    public boolean addEvent(Person person, String eventTyoe, LocalDate date, LocalDate reminderDeadline) {
+        Event event;
+        if (eventTyoe.equals("Birthday")) {
+            event = new Birthday(person, date, reminderDeadline);
+        } else {
+            event = new Anniversary(person, date, reminderDeadline);
         }
-        this.eventMap.put(keyName, val);
+        dataAccessor.addEvent(event);
         return true;
     }
 
     /**
      * Remove an event from the event hash map
-     * @param keyName - the key we are adding to the hashmap
+     * @param firstName - the first name of the person whose events we are removing
+     * @param lastName - the last name of the person whose events we are removing
      * @return false if the key is not in the hashmap and true if we
      * succesfully remove the key and corresponding value
      */
-    public boolean removeEvent(String keyName) {
-        if (eventMap.containsKey(keyName)) {
-            this.eventMap.remove(keyName);
-            return true;
+    public boolean removeEvent(String firstName, String lastName) {
+        List<Event> whichEventsToRemove = dataAccessor.findEvent(firstName, lastName);
+        for (Event e : whichEventsToRemove) {
+            dataAccessor.removeEvent(e);
         }
         return false;
     }
