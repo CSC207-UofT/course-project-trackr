e7ae195ed0311d7734f1ef5b0142836cbd0a0308
danielhocevar <danielhocevar1@gmail.com>
2021-10-15 20:00:07 -0400

    Updated EventManager to conform to newest DataAccess methods, as well as support removing a specific event type in the removeEvent method. Also added null safety to the removeEvent method.

--- trackr/src/main/java/usecases/EventManager.java
+++ trackr/src/main/java/usecases/EventManager.java
@@ -8,6 +8,7 @@ import entities.Person;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 
 /**
  * EventManager is a use case class that manages the creation and deletion of events
@@ -20,7 +21,7 @@ public class EventManager {
         this.dataAccessor = dataAccessorObj;
     }
 
-    public List<Event> getAllEvents() {
+    public Set<Event> getAllEvents() {
         return dataAccessor.getEventData();
     }
 
@@ -45,15 +46,17 @@ public class EventManager {
 
     /**
      * Remove an event from the event hash map
+     * @param eventType - either Birthday or Anniversary
      * @param firstName - the first name of the person whose events we are removing
      * @param lastName - the last name of the person whose events we are removing
      * @return false if no events were removed, and true if one or more events were removed
      */
-    public boolean removeEvent(String firstName, String lastName) {
-        List<Event> whichEventsToRemove = dataAccessor.findEvent(firstName, lastName);
+    public boolean removeEvent(String eventType, String firstName, String lastName) {
+        Event whichEventToRemove = dataAccessor.findEvent(eventType.equals("Birthday") ?
+                EventOutputData.EventTypes.BIRTHDAY : EventOutputData.EventTypes.ANNIVERSARY, firstName, lastName);
         boolean eventRemoved = false;
-        for (Event e : whichEventsToRemove) {
-            dataAccessor.removeEvent(e);
+        if (whichEventToRemove != null) {
+            dataAccessor.removeEvent(whichEventToRemove);
             eventRemoved = true;
         }
         return eventRemoved;
