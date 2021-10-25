d04bcd6cd10af40f4e35106f16c43c91033ec81c
Menghao Yu <menghaoyu2002@gmail.com>
2021-10-25 19:53:57 -0400

    Deleted Old Android

--- trackr-app/app/src/main/java/com/io/trackr/trackr_app/input_output_interfaces/OutputBoundary.java
+++
@@ -1,8 +0,0 @@
-package input_output_interfaces;
-
-/**
- * Interface for allowing higher level classes to send output to lower level classes.
- */
-public interface OutputBoundary {
-    void sendOutput(String output);
-}
