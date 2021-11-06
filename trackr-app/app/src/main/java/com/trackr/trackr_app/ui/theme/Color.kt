b0e0416e642dfbd01273458bafec283f18fbe668
Menghao Yu <menghaoyu2002@gmail.com>
2021-11-06 19:44:24 -0400

    Added default colors and fonts.

--- trackr-app/app/src/main/java/com/trackr/trackr_app/ui/theme/Color.kt
+++ trackr-app/app/src/main/java/com/trackr/trackr_app/ui/theme/Color.kt
@@ -6,3 +6,10 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+// CustomColors
+val DarkBlue = Color(0x060028FF)
+val DarkBlueVariant = Color(0x1F1361FF)
+val LightBlue = Color(0x9DC2CDFF)
+val BackgroundBlue = Color(0x0D0345FF)
+val LightBackground = Color(0x1F1361FF)
--- trackr-app/app/src/main/java/com/trackr/trackr_app/ui/theme/Theme.kt
+++ trackr-app/app/src/main/java/com/trackr/trackr_app/ui/theme/Theme.kt
@@ -5,17 +5,22 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = DarkBlue,
+    primaryVariant = DarkBlueVariant,
+    secondary = LightBlue,
+    onPrimary = Color.White,
+    background = BackgroundBlue
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color.White,
+    primaryVariant = LightBlue,
+    secondary = DarkBlue,
+    onPrimary = DarkBlue,
+    background = LightBackground
 
     /* Other default colors to override
     background = Color.White,
