b92587391b5cbf883b9fc3b1961ec8dfa498c979
Roman Zupancic <roman.k.zupancic@gmail.com>
2021-11-09 16:10:29 -0500

    things

--- trackr-app/app/src/main/java/com/trackr/trackr_app/entities/Event.kt
+++ trackr-app/app/src/main/java/com/trackr/trackr_app/entities/Event.kt
@@ -1,17 +1,11 @@
 package com.trackr.trackr_app.entities
 
-import com.trackr.trackr_app.entities.eventrepeaterstrategy.EventRepeaterStrategy
 import java.time.LocalDate
-import kotlin.jvm.JvmOverloads
+import java.util.*
 
-class Event @JvmOverloads constructor(
-    val person: Person,
+data class Event constructor(
+    val person: UUID,
     val date: LocalDate,
-    val eventRepeaterStrategy: EventRepeaterStrategy
-) {
-    fun getDatesUntil(date: LocalDate): List<LocalDate> =
-        eventRepeaterStrategy.getBetween(this.date, date)
-
-    fun getDatesBetween(to: LocalDate, from: LocalDate): List<LocalDate> =
-        eventRepeaterStrategy.getBetween(to, from)
-}
+    val eventRepeaterStrategy: String,
+    val eventReminderStrategy: String
+)
