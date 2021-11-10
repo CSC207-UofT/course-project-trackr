add552901ae1068d6e614797d9eeb0f2a08c8a2a
nathan hansen <nathan.hansen@mail.utoronto.ca>
2021-11-10 13:29:41 -0500

    fixed db init, adding Person and Event

--- trackr-app/app/src/main/java/com/trackr/trackr_app/database/Database.kt
+++ trackr-app/app/src/main/java/com/trackr/trackr_app/database/Database.kt
@@ -4,14 +4,20 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.trackr.trackr_app.model.Event
+import com.trackr.trackr_app.model.Person
 import com.trackr.trackr_app.model.User
 
 // TODO: add Person::class, Event::class once implemented
 
-@Database(entities = [User::class,], version = 1, exportSchema = false)
+@Database(entities = [User::class, Person::class, Event::class], version = 1, exportSchema = false)
 abstract class TrackrDatabase : RoomDatabase() {
 
     abstract fun userDao(): UserDao
+
+    abstract fun personDao(): PersonDao
+
+    abstract fun eventDao(): EventDao
 
     companion object {
         @Volatile
