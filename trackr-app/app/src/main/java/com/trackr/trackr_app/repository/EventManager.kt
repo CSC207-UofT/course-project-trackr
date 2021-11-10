217de27c7450f1f8b40d12d357ac55fd588dba1e
nathan hansen <nathan.hansen@mail.utoronto.ca>
2021-11-10 13:57:14 -0500

    added minimal managers for object creation and pass-thru for db classes

---
+++ trackr-app/app/src/main/java/com/trackr/trackr_app/repository/EventManager.kt
@@ -0,0 +1,11 @@
+package com.trackr.trackr_app.repository
+
+import com.trackr.trackr_app.model.Event
+import java.sql.Date
+import java.util.UUID
+
+class EventManager {
+}
+fun createEvent(person_id: String, type: Int, date: Date, reminder_interval: Int, repeat_strategy: Int): Event {
+    return Event(UUID.randomUUID().toString(), person_id, type, date, reminder_interval, repeat_strategy)
+}
