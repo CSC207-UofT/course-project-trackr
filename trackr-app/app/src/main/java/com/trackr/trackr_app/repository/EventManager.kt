de993fcef8d9555701f4725d245b6e078da25b9c
Roman Zupancic <roman.k.zupancic@gmail.com>
2021-11-11 10:32:30 -0500

    Proposal for manager-repository data access pattern

--- trackr-app/app/src/main/java/com/trackr/trackr_app/repository/EventManager.kt
+++ trackr-app/app/src/main/java/com/trackr/trackr_app/repository/EventManager.kt
@@ -1,11 +1,27 @@
 package com.trackr.trackr_app.repository
 
 import com.trackr.trackr_app.model.TrackrEvent
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.sql.Date
 import java.util.UUID
 
-class EventManager {
-}
-fun createEvent(person_id: String, type: Int, date: Date, reminder_interval: Int, repeat_strategy: Int): TrackrEvent {
-    return TrackrEvent(UUID.randomUUID().toString(), person_id, type, date, reminder_interval, repeat_strategy)
+class EventManager(val eventRepository: EventRepository) {
+    fun createEvent(person_id: String, type: Int, date: Date, reminder_interval: Int, repeat_strategy: Int): TrackrEvent {
+        val trackrEvent = TrackrEvent(
+            UUID.randomUUID().toString(),
+            person_id,
+            type,
+            date,
+            reminder_interval,
+            repeat_strategy
+        )
+        runBlocking {
+            launch {
+                eventRepository.insert(trackrEvent)
+            }
+        }
+
+        return trackrEvent
+    }
 }
