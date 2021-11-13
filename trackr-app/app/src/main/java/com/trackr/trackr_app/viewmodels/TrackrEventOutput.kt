734bfdfb57fa1b322ec45b958e5cfc5ebfd4cce2
Menghao Yu <menghaoyu2002@gmail.com>
2021-11-13 03:07:41 -0500

    Changed Wrapper class to only take in a name.

--- trackr-app/app/src/main/java/com/trackr/trackr_app/viewmodels/TrackrEventOutput.kt
+++ trackr-app/app/src/main/java/com/trackr/trackr_app/viewmodels/TrackrEventOutput.kt
@@ -4,11 +4,9 @@ import com.trackr.trackr_app.model.Person
 import com.trackr.trackr_app.model.TrackrEvent
 import java.time.LocalDate
 
-class TrackrEventOutput(event: TrackrEvent, person: Person) {
+class TrackrEventOutput(event: TrackrEvent, val name: String) {
     val id = event.id
     val personId = event.person_id
-    val firstName = person.first_name
-    val lastName = person.last_name
     val type = event.type
     val date: LocalDate = LocalDate.ofEpochDay(event.date)
     val reminderInterval = event.reminder_interval
