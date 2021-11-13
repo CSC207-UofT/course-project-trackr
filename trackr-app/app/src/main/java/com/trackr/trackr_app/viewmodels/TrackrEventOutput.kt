a311bb498894c31cff2e352ea236742fa1e6205b
Menghao Yu <menghaoyu2002@gmail.com>
2021-11-13 05:07:00 -0500

    Revert "Changed Wrapper class to only take in a name."
    This reverts commit 734bfdfb

--- trackr-app/app/src/main/java/com/trackr/trackr_app/viewmodels/TrackrEventOutput.kt
+++ trackr-app/app/src/main/java/com/trackr/trackr_app/viewmodels/TrackrEventOutput.kt
@@ -4,9 +4,11 @@ import com.trackr.trackr_app.model.Person
 import com.trackr.trackr_app.model.TrackrEvent
 import java.time.LocalDate
 
-class TrackrEventOutput(event: TrackrEvent, val name: String) {
+class TrackrEventOutput(event: TrackrEvent, person: Person) {
     val id = event.id
     val personId = event.person_id
+    val firstName = person.first_name
+    val lastName = person.last_name
     val type = event.type
     val date: LocalDate = LocalDate.ofEpochDay(event.date)
     val reminderInterval = event.reminder_interval
