8c1e73b4a52a3ff7e46c39360e3ce937f3e75b36
bibloboi <71408986+bibloboi@users.noreply.github.com>
2021-11-13 16:12:52 -0500

    Adjusted to accomodate for most recent merge which was not accounted for in previous commit.

--- trackr-app/app/src/main/java/com/trackr/trackr_app/viewmodels/EditScreenViewModel.kt
+++ trackr-app/app/src/main/java/com/trackr/trackr_app/viewmodels/EditScreenViewModel.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.Observer
 import com.trackr.trackr_app.model.Person
 import com.trackr.trackr_app.model.TrackrEvent
 import com.trackr.trackr_app.model.User
+import com.trackr.trackr_app.notification.EventNotificationManager
 import com.trackr.trackr_app.repository.EventRepository
 import com.trackr.trackr_app.repository.PersonRepository
 import com.trackr.trackr_app.repository.UserRepository
@@ -21,6 +22,8 @@ import javax.inject.Inject
 @HiltViewModel
 class EditScreenViewModel @Inject constructor(
     private val eventRepository: EventRepository,
+    private val personRepository: PersonRepository,
+    private val eventNotificationManager: EventNotificationManager
 ) : ViewModel() {
 
     private val _delete = mutableStateOf("No")
@@ -92,6 +95,7 @@ class EditScreenViewModel @Inject constructor(
      */
     fun editEvent(id: String) = viewModelScope.launch {
         val event = eventRepository.getById(id)
+        val person = personRepository.getPersonById(event.person_id)
         val reminderInt: Int? = mapOf("1 day before" to 1, "3 days before" to 3,
                 "1 week before" to 7, "2 weeks before" to 14,
                 "1 month before" to 30)[chosenReminder.value]
@@ -99,11 +103,22 @@ class EditScreenViewModel @Inject constructor(
         eventRepository.editDate(eventDate.value.withYear(1970), event)
 
         eventRepository.editType(eventType, event)
+
+        //Edit notification
+        eventNotificationManager.editNotification(
+                "${person.first_name} ${person.last_name}",
+                eventName.value,
+                eventDate.value,
+                event.id.hashCode()
+        )
     }
 
     fun deleteEvent(id: String) = viewModelScope.launch {
         val event = eventRepository.getById(id)
         eventRepository.delete(event)
+
+        //Delete notification
+        eventNotificationManager.removeNotification(event.id.hashCode())
     }
 }
 
