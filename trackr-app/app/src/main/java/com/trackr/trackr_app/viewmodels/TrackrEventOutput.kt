2650e578acb1fea72ea6e0bfa1df4c9bf38e345b
Menghao Yu <menghaoyu2002@gmail.com>
2021-11-13 00:05:38 -0500

    Created A wrapper event.

---
+++ trackr-app/app/src/main/java/com/trackr/trackr_app/viewmodels/TrackrEventOutput.kt
@@ -0,0 +1,13 @@
+package com.trackr.trackr_app.viewmodels
+
+import com.trackr.trackr_app.model.TrackrEvent
+import java.time.LocalDate
+
+class TrackrEventOutput(event: TrackrEvent) {
+    val id = event.id
+    val personId = event.person_id
+    val type = event.type
+    val date: LocalDate = LocalDate.ofEpochDay(event.date)
+    val reminderInterval = event.reminder_interval
+    val repeatStrategy = event.repeat_strategy
+}
