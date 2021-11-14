ee909df0208ac4676857d786205f4b9ce2dd96b1
bibloboi <71408986+bibloboi@users.noreply.github.com>
2021-11-13 20:20:55 -0500

    Added tests for notifications

--- trackr-app/app/src/androidTest/java/com/trackr/trackr_app/notification/EventNotificationManagerTest.kt
+++ trackr-app/app/src/androidTest/java/com/trackr/trackr_app/notification/EventNotificationManagerTest.kt
@@ -1,21 +1,21 @@
 package com.trackr.trackr_app.notification
 
 import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.app.PendingIntent.FLAG_NO_CREATE
 import android.content.Context
 import android.content.Intent
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.*
 
-import org.junit.After
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.time.LocalDate
 
 @RunWith(AndroidJUnit4::class)
 class EventNotificationManagerTest {
-    private val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
+    private val context: Context = ApplicationProvider.getApplicationContext()
     private val enm: EventNotificationManager = EventNotificationManager(context)
 
     @Test
@@ -23,11 +23,12 @@ class EventNotificationManagerTest {
         val id = 100
         enm.createNotification(
                 "John", "Birthday",
-                LocalDate.of(2022, 10, 10), id)
+                LocalDate.of(2022, 10, 10),
+                LocalDate.of(2022, 10, 9), id)
         val intent = Intent(context, EventBroadcastReceiver::class.java)
         val pendingIntent =
                 PendingIntent.getBroadcast(
-                        context, id, intent, PendingIntent.FLAG_NO_CREATE)
+                        context, id, intent, FLAG_IMMUTABLE or FLAG_NO_CREATE)
         assertNotEquals(null, pendingIntent)
     }
 
@@ -36,12 +37,13 @@ class EventNotificationManagerTest {
         val id = 200
         enm.createNotification(
                 "Jason", "Birthday",
-                LocalDate.of(2022, 10, 10), id)
+                LocalDate.of(2022, 10, 10),
+                LocalDate.of(2022, 10, 9), id)
         enm.removeNotification(id)
         val intent = Intent(context, EventBroadcastReceiver::class.java)
         val pendingIntent =
                 PendingIntent.getBroadcast(
-                        context, id, intent, PendingIntent.FLAG_NO_CREATE)
+                        context, id, intent, FLAG_IMMUTABLE or FLAG_NO_CREATE)
         assertEquals(null, pendingIntent)
     }
 }
