5fcc2ed5a6690917fa161bf33101db8237e371a4
Jonathan <jonathanginevro@outlook.com>
2021-11-14 18:13:42 -0500

    frame

---
+++ trackr-app/app/src/androidTest/java/com/trackr/trackr_app/viewmodels/EditScreenViewModelTest.kt
@@ -0,0 +1,32 @@
+package com.trackr.trackr_app.viewmodels
+
+import junit.framework.TestCase
+
+class EditScreenViewModelTest : TestCase() {
+
+    fun testGetEventName() {}
+
+    fun testGetEventDate() {}
+
+    fun testGetChosenReminder() {}
+
+    fun testGetPersonName() {}
+
+    fun testEditEventName() {}
+
+    fun testChangeMonth() {}
+
+    fun testChangeDay() {}
+
+    fun testChangeYear() {}
+
+    fun testChangeReminderInterval() {}
+
+    fun testGetMonths() {}
+
+    fun testGetReminderIntervals() {}
+
+    fun testEditEvent() {}
+
+    fun testDeleteEvent() {}
+}
