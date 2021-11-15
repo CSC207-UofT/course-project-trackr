0fe5b5e98d4364401a3e62812f4c2b83f8aaaec1
Menghao Yu <menghaoyu2002@gmail.com>
2021-11-15 00:38:45 -0500

    Added Singleton Annotation

--- trackr-app/app/src/main/java/com/trackr/trackr_app/manager/PersonManager.kt
+++ trackr-app/app/src/main/java/com/trackr/trackr_app/manager/PersonManager.kt
@@ -2,7 +2,9 @@ package com.trackr.trackr_app.manager
 
 import com.trackr.trackr_app.model.Person
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class PersonManager @Inject constructor() {
     fun createPerson(id: String, first_name: String, last_name: String): Person {
         return Person(
