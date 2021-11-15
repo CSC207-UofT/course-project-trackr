d48e456f3bfe09f5115bd350d0e414b4b2358121
bibloboi <71408986+bibloboi@users.noreply.github.com>
2021-11-15 01:02:23 -0500

    Update AddScreenViewModelTest
    Update setup since EventManager and PersonManager are required for AddScreenViewModel.

--- trackr-app/app/src/androidTest/java/com/trackr/trackr_app/viewmodels/AddScreenViewModelTest.kt
+++ trackr-app/app/src/androidTest/java/com/trackr/trackr_app/viewmodels/AddScreenViewModelTest.kt
@@ -6,6 +6,8 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.trackr.trackr_app.database.TrackrDatabase
+import com.trackr.trackr_app.manager.EventManager
+import com.trackr.trackr_app.manager.PersonManager
 import com.trackr.trackr_app.notification.EventNotificationManager
 import com.trackr.trackr_app.repository.EventRepository
 import com.trackr.trackr_app.repository.PersonRepository
@@ -37,7 +39,9 @@ class AddScreenViewModelTest : TestCase() {
         eventRepository = EventRepository(db.eventDao())
         val eventNotificationManager = EventNotificationManager(context)
         viewModel = AddScreenViewModel(
-                eventRepository, personRepository, userRepository, eventNotificationManager)
+                eventRepository, personRepository, userRepository, eventNotificationManager,
+                EventManager(), PersonManager()
+        )
     }
 
     @After
