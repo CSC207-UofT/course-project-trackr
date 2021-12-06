2bb3e2d1618f1f179f452f2787f54b6f61e20523
bibloboi <71408986+bibloboi@users.noreply.github.com>
2021-12-05 22:22:29 -0500

    Added PersonDetailsScreenViewModelTest.kt

--- trackr-app/app/src/androidTest/java/com/trackr/trackr_app/viewmodels/PersonDetailsScreenViewModelTest.kt
+++ trackr-app/app/src/androidTest/java/com/trackr/trackr_app/viewmodels/PersonDetailsScreenViewModelTest.kt
@@ -1,17 +1,20 @@
 package com.trackr.trackr_app.viewmodels
 
 import android.content.Context
+import androidx.lifecycle.SavedStateHandle
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.trackr.trackr_app.database.TrackrDatabase
 import com.trackr.trackr_app.manager.PersonManager
 import com.trackr.trackr_app.manager.UserManager
+import com.trackr.trackr_app.model.Person
+import com.trackr.trackr_app.model.User
+import com.trackr.trackr_app.repository.EventRepository
 import com.trackr.trackr_app.repository.PersonRepository
 import com.trackr.trackr_app.repository.UserRepository
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.*
 
 import org.junit.Before
@@ -19,51 +22,75 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class AddPersonViewModelTest {
+class PersonDetailsScreenViewModelTest {
     private lateinit var db: TrackrDatabase
+    private lateinit var person: Person
     private lateinit var personRepository: PersonRepository
-    private lateinit var viewModel: AddPersonViewModel
+    private lateinit var viewModel: PersonDetailsScreenViewModel
 
     @Before
-    fun setUp() {
+    fun setUp() = runBlocking {
         val context = ApplicationProvider.getApplicationContext<Context>()
         db = Room.inMemoryDatabaseBuilder(context, TrackrDatabase::class.java)
                 .allowMainThreadQueries().build()
         val userRepository = UserRepository(db.userDao())
         personRepository = PersonRepository(db.personDao())
+        val eventRepository = EventRepository(db.eventDao())
         val userManager = UserManager(userRepository)
         val personManager = PersonManager(personRepository, userManager)
-        viewModel = AddPersonViewModel(personManager)
+
+        val user = User("test")
+        userRepository.insert(user)
+        person = Person(user.id, "tom", "sawyer")
+        personRepository.insert(person)
+
+        val state = SavedStateHandle()
+        state.set("personId", person.id)
+
+        viewModel = PersonDetailsScreenViewModel(personManager,
+                eventRepository,
+                personManager,
+                state)
     }
 
     @Test
     fun editFirstName() = runBlocking {
-        viewModel.editFirstName("John").join()
+        viewModel.editFirstName("Timothy").join()
 
         val result = viewModel.firstName.value
-        val expected = "John"
+        val expected = "Timothy"
 
         assertEquals(expected, result)
     }
 
     @Test
     fun editLastName() = runBlocking {
-        viewModel.editLastName("Xina").join()
+        viewModel.editLastName("Laurelle").join()
 
         val result = viewModel.lastName.value
-        val expected = "Xina"
+        val expected = "Laurelle"
 
         assertEquals(expected, result)
     }
 
     @Test
-    fun addPerson() = runBlocking {
-        viewModel.editFirstName("John").join()
-        viewModel.editLastName("Xina").join()
-        viewModel.addPerson().join()
+    fun editPerson() = runBlocking {
+        viewModel.editFirstName("Timothy").join()
+        viewModel.editLastName("Laurelle").join()
+        viewModel.editPerson().join()
 
         val result = personRepository.getAllPersons().first()[0]
-        assertEquals("John", result.firstName)
-        assertEquals("Xina", result.lastName)
+        assertEquals("Timothy", result.firstName)
+        assertEquals("Laurelle", result.lastName)
+    }
+
+    @Test
+    fun deletePerson() = runBlocking {
+        viewModel.deletePerson().join()
+
+        val result = personRepository.getAllPersons().first()
+        val expected: List<Person> = emptyList()
+
+        assertEquals(expected, result)
     }
 }
